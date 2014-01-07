627b27bd94ba575892c92b3a1b0264b9a24a4609
mprabtani <mehul.prabtani@techgrains.com>
2014-01-07 12:36:05 +0530

    Added Image

--- SampleApp/ViewController.m
+++ SampleApp/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     //TEST comment
+    //After Adding Image
 }
 
 - (void)didReceiveMemoryWarning
